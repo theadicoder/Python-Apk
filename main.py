dcf09aaef91ce5bf2b4683e1c86f4b662776619f
ADITYA THAKUR <Aadicorian@outlook.com>
2024-04-02 06:40:37 +0000

    Code Updated

---
+++ main.py
@@ -0,0 +1,8 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+class MainApp(MDApp):
+    def build(self):
+        return MDLabel(text="Hey DevOps Welcome to theadicoder",halign="center")
+        if_name_=="_main_":
+            Main_App().run()
